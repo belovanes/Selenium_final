548cf7aee16de4aeeaae39a4973202eaaada73e5
belovanes <gogagog@mail.ru>
2022-04-14 09:54:30 +0300

    предыдущий коммит был ошибочным. исправлено

--- test_product_page.py
+++ test_product_page.py
@@ -51,7 +51,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-def should_be_login_page()_product_page(browser):
+def test_guest_can_go_to_login_page_from_product_page(browser):
     LINK = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, LINK)
     page.open()
